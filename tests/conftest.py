948fd1c9e574054c74b5d86911ebd3a5a9a05436
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-12-20 10:53:10 +0300

    [delivers #162753229] Write tests for creating a red flag record

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,29 +3,23 @@
 EMPTY_RECORD = {}
 
 REPORT = {
-    'id' :1,
     'type': 'red-flag',
     'location':"5555,555",
-    'status' : 'pending',
     'images':'images',
     'videos':'videos',
     'comment':'lorem ipsum doe'
     }
 
 MISSING_KEYS = {
-    'id' :1,
     'location':"5555,555",
-    'status' : 'pending',
     'images':'images',
     'videos':'videos',
     'comment':'lorem ipsum doe'
     }
 
 EMPTY_FIELDS = {
-    'id' :1,
-    'type': 'red-flag',
+    'type': '',
     'location':"5555,555",
-    'status' : 'pending',
     'images':'images',
     'videos':'videos',
     'comment':'lorem ipsum doe'
