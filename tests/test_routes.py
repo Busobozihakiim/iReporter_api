44c5198eec71197ff1ac10f438ac3c7b5dfea638
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-12-20 15:09:04 +0300

    [delivers #162753386 ] write tests for feature that edits location of a record

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,6 +22,14 @@ def test_gets_records_when_null(_setup):
     assert response.status_code == 404
     assert 'You dont have any red flag records' in str(response.json)
 
+def test_change_redflags_location_with_no_records(_setup):
+    """test change of location when no red flgs available"""
+    response = _setup.patch('/api/v1/red_flags/1/location',
+                            data=json.dumps({"location":"32.565, 26.356"}),
+                            content_type='application/json')
+    assert response.status_code == 404
+    assert 'You have no red flag records' in str(response.json)
+
 def test_create_record_with_no_data(_setup):
     """Test whether the user has submitted an empty post"""
     response = _setup.post('/api/v1/red_flags',
@@ -71,3 +79,33 @@ def test_gets_records(_setup):
     response = _setup.get('/api/v1/red_flags')
     assert response.status_code == 200
     assert 'lorem ipsum doe' in str(response.json)
+
+def test_edits_records_location(_setup):
+    """test change of location whe nothing has been posted"""
+    response = _setup.patch('/api/v1/red_flags/2/location')
+    assert response.status_code == 400
+    assert 'You entered nothing' in str(response.json)
+
+def test_edits_records_location_no_cordinates(_setup):
+    """test change of location with no cordinates """
+    response = _setup.patch('/api/v1/red_flags/2/location',
+                            data=json.dumps({"location":""}),
+                            content_type='application/json')
+    assert response.status_code == 400
+    assert 'You have not entered the cordinates' in str(response.json)
+
+def test_change_redflags_location(_setup):
+    """test change of location """
+    response = _setup.patch('/api/v1/red_flags/1/location',
+                            data=json.dumps({"location":"32.565, 26.356"}),
+                            content_type='application/json')
+    assert response.status_code == 201
+    assert 'Updated red-flag record’s location' in str(response.json)
+
+def test_edits_records_location_when_id_not_exists(_setup):
+    """test change of location when the record is not available"""
+    response = _setup.patch('/api/v1/red_flags/2/location',
+                            data=json.dumps({"location":"32.565, 26.356"}),
+                            content_type='application/json')
+    assert response.status_code == 404
+    assert 'The red flag of id 2 does not exist' in str(response.json)
